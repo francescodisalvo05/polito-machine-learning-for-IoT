055672682dc1af6815b2c7c4701f0651d776b016
Francesco Di Salvo <francesco.disalvo99@gmail.com>
2021-10-28 08:25:47 +0200

    Compute the MFCCs from the log scaled spectrogram

--- Labs/Lab_02/ex3.py
+++ Labs/Lab_02/ex3.py
@@ -21,8 +21,8 @@ def get_mfcc(args):
 	print('--- Transforming the serialized tensor into a tensor ---')
 	spectrogram = tf.io.parse_tensor(byte_spectrogram, out_type=tf.float32)
 
-	# compute the Mel spectrogram with 40mel bins, 20Hz as lower freq and 4kHz as upper freq
-	print('--- Extracting the spectrogram from the stft ---')
+	# compute the log scaled Mel spectrogram with 40mel bins, 20Hz as lower freq and 4kHz as upper freq
+	print('--- Extracting the log scaled Mel spectrogram from the stft ---')
 	time_mfcc = time.time()
 
 	num_spectrogram_bins = spectrogram.shape[-1]
@@ -37,6 +37,26 @@ def get_mfcc(args):
 	mel_spectrogram.set_shape(spectrogram.shape[:-1].concatenate(linear_to_mel_weight_matrix.shape[-1:]))
 
 	log_mel_spectrogram = tf.math.log(mel_spectrogram + 1e-6)
+	print(f'--- Log scaled Mel spectrogram extracted in {time.time()-time_mfcc} seconds ---')
+
+	# compute the MFCCs from the log scaled mel spectrogram and take the first 10 coefficients
+	print('--- Computing the MFCCs ---')
+	time_mfcc = time.time()
+	mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)[,:10]
+	print(f'--- Computed the MFCCs in {time.time() - time_mfcc} seconds --- ')
+
+	# convert the MFCCs in byte tensor and write it on disk
+	mfccs_byte_tensor = tf.io.serialize_tensor(spectrogram)
+	print('--- Convert it in bytes ---')
+
+	# save it on disk
+	mfccs_filename = args.base_path + "mfccs_res_" + args.filename
+	tf.io.write_file(filename=mfccs_filename, contents=mfccs_byte_tensor)
+	print('--- Saved the MFCCs on disk ---')
+
+	# get the sizes
+	print(f'--- The size of the MFCCs is {os.path.getsize(mfccs_filename} ---')
+	print(f'--- The size of the spectrogra is {os.path.getsize(args.base_path + args.filename} ---')
 
 def main():
 
