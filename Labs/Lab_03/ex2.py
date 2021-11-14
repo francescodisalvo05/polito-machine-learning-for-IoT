8b303128c12ed07aeda1257ffbfc9bf4d2674e52
francescodisalvo05 <francesco.disalvo99@gmail.com>
2021-11-14 20:01:51 +0100

    Add ex 2.1

--- Labs/Lab_03/ex2.py
+++ Labs/Lab_03/ex2.py
@@ -7,3 +7,25 @@ has a variable duration (1s the longest).
 Write a Python script to train and evaluate different models for keyword 
 spotting on the Mini Speech Command dataset.
 """
+
+import argparse
+import numpy as np
+import os
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+
+
+def load_data():
+    mini_speech_command_zip = tf.keras.utils.get_file(
+                            origin = "http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
+                            fname = "mini_speech_commands.zip",
+                            extract=True,
+                            cache_dir='.',
+                            cache_subdir='data'
+                        )
+    # it will download a folder and its subfolders
+    # data > mini_speech_commands > [down/, go/, left/, no/, right/, stop/, us/, yes/]
+    csv_path, _ = os.path.splitext(mini_speech_command_zip)
+
+
