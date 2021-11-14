167eb577748d1716ac94b96cfcd57ec22a32666b
francescodisalvo05 <francesco.disalvo99@gmail.com>
2021-11-14 10:37:24 +0100

    Init ex 2

---
+++ Labs/Lab_03/ex2.py
@@ -0,0 +1,9 @@
+"""
+The Mini Speech Command dataset collects 8000 samples of eight keywords 
+('down', 'no', 'go', 'yes', 'stop', 'up', 'right', 'left'), 1000 samples 
+per label. Each sample is recorded at 16kHz with an Int16 resolution and 
+has a variable duration (1s the longest).
+
+Write a Python script to train and evaluate different models for keyword 
+spotting on the Mini Speech Command dataset.
+"""
