12ea3d2254c5e65a874e73307ecec1e170b66457
francescodisalvo05 <francesco.disalvo99@gmail.com>
2021-12-03 22:19:56 +0100

    Fix architecture ex 02

--- Labs/Lab_03/ex2.py
+++ Labs/Lab_03/ex2.py
@@ -16,162 +16,194 @@ import tensorflow as tf
 from tensorflow import keras
 
 
-def load_data():
-    mini_speech_command_zip = tf.keras.utils.get_file(
+class SignalGenerator:
+    def __init__(self, labels, sampling_rate, frame_length, frame_step,
+            num_mel_bins=None, lower_frequency=None, upper_frequency=None,
+            num_coefficients=None, mfcc=False):
+        self.labels = labels
+        self.sampling_rate = sampling_rate
+        self.frame_length = frame_length
+        self.frame_step = frame_step
+        self.num_mel_bins = num_mel_bins
+        self.lower_frequency = lower_frequency
+        self.upper_frequency = upper_frequency
+        self.num_coefficients = num_coefficients
+        num_spectrogram_bins = (frame_length) // 2 + 1
+
+        if mfcc is True:
+            self.linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
+                    self.num_mel_bins, num_spectrogram_bins, self.sampling_rate,
+                    self.lower_frequency, self.upper_frequency)
+            self.preprocess = self.preprocess_with_mfcc
+        else:
+            self.preprocess = self.preprocess_with_stft
+
+    def read(self, file_path):
+        parts = tf.strings.split(file_path, os.path.sep)
+        label = parts[-2]
+        label_id = tf.argmax(label == self.labels)
+        audio_binary = tf.io.read_file(file_path)
+        audio, _ = tf.audio.decode_wav(audio_binary)
+        audio = tf.squeeze(audio, axis=1)
+
+        return audio, label_id
+
+    def pad(self, audio):
+        zero_padding = tf.zeros([self.sampling_rate] - tf.shape(audio), dtype=tf.float32)
+        audio = tf.concat([audio, zero_padding], 0)
+        audio.set_shape([self.sampling_rate])
+
+        return audio
+
+    def get_spectrogram(self, audio):
+        stft = tf.signal.stft(audio, frame_length=self.frame_length,
+                frame_step=self.frame_step, fft_length=self.frame_length)
+        spectrogram = tf.abs(stft)
+
+        return spectrogram
+
+    def get_mfccs(self, spectrogram):
+        mel_spectrogram = tf.tensordot(spectrogram,
+                self.linear_to_mel_weight_matrix, 1)
+        log_mel_spectrogram = tf.math.log(mel_spectrogram + 1.e-6)
+        mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)
+        mfccs = mfccs[..., :self.num_coefficients]
+
+        return mfccs
+
+    def preprocess_with_stft(self, file_path):
+        audio, label = self.read(file_path)
+        audio = self.pad(audio)
+        spectrogram = self.get_spectrogram(audio)
+        spectrogram = tf.expand_dims(spectrogram, -1)
+        spectrogram = tf.image.resize(spectrogram, [32, 32])
+
+        return spectrogram, label
+
+    def preprocess_with_mfcc(self, file_path):
+        audio, label = self.read(file_path)
+        audio = self.pad(audio)
+        spectrogram = self.get_spectrogram(audio)
+        mfccs = self.get_mfccs(spectrogram)
+        mfccs = tf.expand_dims(mfccs, -1)
+
+        return mfccs, label
+
+    def make_dataset(self, files, train):
+        ds = tf.data.Dataset.from_tensor_slices(files)
+        ds = ds.map(self.preprocess, num_parallel_calls=4)
+        ds = ds.batch(32)
+        ds = ds.cache()
+        if train is True:
+            ds = ds.shuffle(100, reshuffle_each_iteration=True)
+
+        return ds
+
+
+
+def load_data(args):
+
+    if args.silence is True:
+      data_dir = os.path.join('.', 'data', 'mini_speech_commands_silence')
+    else:
+      mini_speech_command_zip = tf.keras.utils.get_file(
                             origin = "http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
                             fname = "mini_speech_commands.zip",
                             extract=True,
                             cache_dir='.',
                             cache_subdir='data'
                         )
-    # it will download a folder and its subfolders
-    # data > mini_speech_commands > [down/, go/, left/, no/, right/, stop/, us/, yes/]
-    csv_path, _ = os.path.splitext(mini_speech_command_zip)
+      # it will download a folder and its subfolders
+      # data > mini_speech_commands > [down/, go/, left/, no/, right/, stop/, us/, yes/]
+      csv_path, _ = os.path.splitext(mini_speech_command_zip)
 
+      data_dir = os.path.join('.', 'data', 'mini_speech_commands')
 
-class SignalGenerator:
-    """Python class called SignalGenerator to implement the data 
-    preparation and preprocessing steps."""
-    def __init__(self, 
-                keywords, 
-                sampling_rate,
-                frame_length,
-                frame_step,
-                num_mel_bins,
-                low_freq,
-                up_freq,
-                num_coefficients,
-                bool_mfcc):
-        self.keywords = keywords
-        self.sampling_rate = sampling_rate
-        self.frame_length = frame_length
-        self.frame_step = frame_step
-        self.num_mel_bins = num_mel_bins
-        self.low_freq = low_freq
-        self.up_freq = up_freq
-        self.num_coefficients = num_coefficients
-        self.bool_mfcc = bool_mfcc
+    filenames = tf.io.gfile.glob(str(data_dir) + '/*/*')
+    filenames = tf.random.shuffle(filenames)
+    num_samples = len(filenames)
 
-
-    def get_raw_dataset(self, path):
-
-        labels = []
-        path_files = []
-
-        for keyword in self.keywords:
-            for audio_file in os.listdir(path + "/" + keyword):
-                path_files.append(path + "/" + keyword + "/" + audio_file)
-                labels.append(keyword)
-
-        path_file_ds = tf.data.Dataset.from_tensor_slices(path_files)
-        label_ds = tf.data.Dataset.from_tensor_slices(labels)
-        
-        return tf.data.Dataset.zip((path_file_ds, label_ds))
-
-
-    def load_and_preprocess_audio(self, audio_path, label):
-
-        # get the tf audio for each recording
-        audio = tf.io.read_file(audio_path)
-        tf_audio, rate = tf.audio.decode_wav(audio)
-        tf_audio = tf.squeeze(tf_audio, 1)
-
-        # to do : pad the audio to 1s
-
-        frame_length = int(self.frame_length * 1e-3 * 16000)
-        frame_step = int(self.frame_step * 1e-3 * 16000)
-
-        # compute the stft
-        stft = tf.signal.stft(tf_audio, frame_length, frame_step, fft_length=frame_length)
-
-        # compute the stft
-        stft = tf.signal.stft(tf_audio, frame_length, frame_step, fft_length=frame_length)
-
-        if not self.bool_mfcc:
-            stft = tf.image.resize(stft, [32,32])
-
-        else:
-            # compute the MFCC
-
-            # extract the spectrogram
-            spectrogram = tf.abs(stft)
-            num_spectrogram_bins = spectrogram.shape[-1]
-            linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
-                                                            self.num_mel_bins, 
-                                                            num_spectrogram_bins, 
-                                                            self.sampling_rate, # sampling rate 
-                                                            self.low_freq, 
-                                                            self.up_freq
-                                                    )
-
-            mel_spectrogram = tf.tensordot(spectrogram, linear_to_mel_weight_matrix, 1)
-            log_mel_spectrogram = tf.math.log(mel_spectrogram + 1.e-6)
-            mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)
-            mfccs = mfccs[..., :self.num_coefficients]
-
-            return mfccs, label 
-        else:
-            return stft, label
-
-
-    def make_dataset(self,path):
-
-        ds = self.get_raw_dataset(path)
-        ds = ds.shuffle(200)
-        ds = ds.map(self.load_and_preprocess_audio)
-
-        return ds
-
-
-def get_model(model_name, mfcc):
-
-    if mfcc:
-        strides = [2,1]
+    if args.silence is True:
+        total = 9000
     else:
-        strides = [2,2]
+        total = 8000
+
+    train_files = filenames[:int(total*0.8)]
+    val_files = filenames[int(total*0.8): int(total*0.9)]
+    test_files = filenames[int(total*0.9):]
+
+
+    LABELS = np.array(tf.io.gfile.listdir(str(data_dir)))
+    LABELS = LABELS[LABELS != 'README.md']
+
+    STFT_OPTIONS = {'frame_length': 256, 'frame_step': 128, 'mfcc': False}
+    MFCC_OPTIONS = {'frame_length': 640, 'frame_step': 320, 'mfcc': True,
+            'lower_frequency': 20, 'upper_frequency': 4000, 'num_mel_bins': 40,
+            'num_coefficients': 10}
+
+
+    if args.mfcc is True:
+        options = MFCC_OPTIONS
+    else:
+        options = STFT_OPTIONS
+
+    generator = SignalGenerator(LABELS, 16000, **options)
+    train_ds = generator.make_dataset(train_files, True)
+    val_ds = generator.make_dataset(val_files, False)
+    test_ds = generator.make_dataset(test_files, False)
+
+    return train_ds, val_ds, test_ds
+
+
+def get_model(model_name, mfcc, silence):
+
+    strides = [2,1] if mfcc else [2,2]
+    units = 9 if silence else 8
 
     if model_name == "MLP":
         model = keras.Sequential([
             keras.layers.Flatten(),
-            keras.layers.Dense(256, activation='relu', name='first_dense'),
-            keras.layers.Dense(256, activation='relu', name='second_dense'),
-            keras.layers.Dense(256, activation='relu', name='third_dense'),
-            keras.layers.Dense(8, name='fourth_dense')
+            keras.layers.Dense(256, activation='relu'),
+            keras.layers.Dense(256, activation='relu'),
+            keras.layers.Dense(256, activation='relu'),
+            keras.layers.Dense(units=units)
         ])
 
     elif model_name == "CNN":
 
         model = keras.Sequential([
-            keras.layers.Conv2D(filters=128, kernel_size=[3,3], stride=strides, use_bias=False),
-            keras.layers.BatchNormalization(momentum=0.1),
-            keras.layers.ReLU(),
-            keras.layers.Conv2D(filters=128, kernel_size=[3,3], stride=[1,1], use_bias=False),
-            keras.layers.BatchNormalization(momentum=0.1),
-            keras.layers.ReLU(),
-            keras.layers.Conv2D(filters=128, kernel_size=[3,3], stride=[1,1], use_bias=False),
-            keras.layers.BatchNormalization(momentum=0.1),
-            keras.layers.ReLU(),
-            keras.layers.GlobalAveragePooling2D,
-            keras.layers.Dense(8),
-        ])
+          keras.layers.Conv2D(filters=128, kernel_size=[3,3], strides=strides, use_bias=False),
+          keras.layers.BatchNormalization(momentum=0.1),
+          keras.layers.ReLU(),
+          keras.layers.Conv2D(filters=128, kernel_size=[3,3], strides=strides, use_bias=False),
+          keras.layers.BatchNormalization(momentum=0.1),
+          keras.layers.ReLU(),
+          keras.layers.Conv2D(filters=128, kernel_size=[3,3], strides=strides, use_bias=False),
+          keras.layers.BatchNormalization(momentum=0.1),
+          keras.layers.ReLU(),
+          keras.layers.GlobalAveragePooling2D(),
+          keras.layers.Dense(units=units)
+      ])
 
     elif model_name == "DS-CNN":
-
         model = keras.Sequential([
-            keras.layers.Conv2D(filters=256, kernel_size=[3,3], stride=strides, use_bias=False),
-            keras.layers.BatchNormalization(momentum=0.1),
-            keras.layers.ReLU(),
-            keras.layers.DepthwiseConv2D(kernel_size=[3, 3], stride=[1, 1], use_bias=False),
-            keras.layers.Conv2D(filters=128, kernel_size=[3,3], stride=[1,1], use_bias=False),
-            keras.layers.BatchNormalization(momentum=0.1),
-            keras.layers.ReLU(),
-            keras.layers.Conv2D(filters=128, kernel_size=[3,3], stride=[1,1], use_bias=False),
-            keras.layers.BatchNormalization(momentum=0.1),
-            keras.layers.ReLU(),
-            keras.layers.GlobalAveragePooling2D,
-            keras.layers.Dense(8),
-        ])
+                  keras.layers.Conv2D(filters=256, kernel_size=[3, 3], strides=strides, use_bias=False),
+                  keras.layers.BatchNormalization(momentum=0.1),
+                  keras.layers.ReLU(),
 
+                  keras.layers.DepthwiseConv2D(kernel_size=[3, 3], strides=[1, 1], use_bias=False),
+                  keras.layers.Conv2D(filters=256, kernel_size=[1, 1], strides=[1, 1], use_bias=False),
+                  keras.layers.BatchNormalization(momentum=0.1),
+                  keras.layers.ReLU(),
+
+                  keras.layers.DepthwiseConv2D(kernel_size=[3, 3], strides=[1, 1], use_bias=False),
+                  keras.layers.Conv2D(filters=256, kernel_size=[1, 1], strides=[1, 1], use_bias=False),
+                  keras.layers.BatchNormalization(momentum=0.1),
+                  keras.layers.ReLU(),
+
+                  keras.layers.GlobalAveragePooling2D(),
+                  keras.layers.Dense(units=units)
+              ])
 
     return model
 
@@ -183,30 +215,41 @@ def train_model(model, train_data, val_data, optimizer, loss, metrics, bs, epoch
 
     model.fit(train_data, validation_data = val_data, batch_size=bs, epochs=epochs)
 
-################################
-########### MAIN
-################################
+def main(args):
+    
+    train_ds, val_ds, test_ds = load_data(args)
 
-signalgenerator = SignalGenerator(
-               keywords = ['down','go','left','no','right','stop','up','yes'], 
-               sampling_rate = 16000,
-               frame_length = 16,
-               frame_step = 8,
-               num_mel_bins = None,
-               low_freq = None,
-               up_freq = None,
-               num_coefficients = None,
-               bool_mfcc = False)
+    # set the output path
+    modality = 'STFT' if not args.mfcc else 'MFCCS'
+    output_path = f'{args.model}_{modality}/'
 
-# dataset containing all the data (before the split)
-ds = signalgenerator.make_dataset('/content/data/mini_speech_commands')
+    model = get_model(args.model, args.mfcc, args.silence)
 
-# split the dataset through take / skip
-N = len(ds)
-train_idx, val_idx = int(N*0.8), int(N*0.9)
+    model.compile(optimizer=tf.optimizers.Adam(),
+                  loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=keras.metrics.SparseCategoricalAccuracy())
 
-train_data = ds.take(train_idx)
-val_data = ds.skip(train_idx).take(val_idx - train_idx)
-test_data = ds.skip(val_idx).take(N - val_idx)
+    save_model = keras.callbacks.ModelCheckpoint(filepath=output_path, 
+                                                monitor="val_sparse_categorical_accuracy",
+                                                save_best_only=True,
+                                                save_weights_only=False,
+                                                save_freq='epoch')
 
-# to do: use three different instances of SignalGenerator
+    model.fit(train_ds, validation_data=val_ds, epochs=args.epochs, callbacks=save_model)
+    test_loss, test_acc = model.evaluate(test_ds)
+    
+    print(f"Test loss: {test_loss:.4f} - Test accuracy: {test_acc:.4f}")
+
+
+if __name__=="__main__":
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-m', '--model', type=str, default="MLP", choices=['MLP','CNN','DS-CNN'])
+    parser.add_argument('-e', '--epochs', type=int, default=20)
+    parser.add_argument('-M','--mfcc', action='store_true')
+    parser.add_argument("-s", '--silence', action='store_true')
+    
+    args = parser.parse_args()
+
+    main(args)
